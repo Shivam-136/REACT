6aca74ca8f9838b5fd489b20babe20fe1fff98c0
Shivam-136 <shivam243028@gmail.com>
2026-04-17 21:13:46 +0530

    Basic-Router-Project

--- React-Router-02/src/components/NavBar.jsx
+++ React-Router-02/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router'
 
 const Navbar = () => {
     return (
-        <div className='flex justify-between items-centerp p-6 shadow-md bg-blue-800'>
+        <div className='flex justify-between items-centerp p-6 shadow-md bg-blue-700'>
             <h1 className='bg-black text-white px-2 p-2 rounded-2xl'>SHIVAM_K</h1>
             <div className='flex gap-10'>
                 <NavLink className={({ isActive }) => (isActive ? "text-red-500 border border-1.2 p-2 rounded-2xl" : "")} to="/"> Home </NavLink>
