80ca0905cbce188752de3d3e013f3e305987a0c3
Shivam-136 <shivam243028@gmail.com>
2026-04-17 21:16:57 +0530

    Basic-Router-Project

--- React-Router-02/src/components/NavBar.jsx
+++ React-Router-02/src/components/NavBar.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { NavLink } from 'react-router'
 
 const Navbar = () => {
+
+    
     return (
         <div className='flex justify-between items-centerp p-6 shadow-md bg-blue-700'>
             <h1 className='bg-black text-white px-2 p-2 rounded-2xl'>SHIVAM_K</h1>
             <div className='flex gap-10'>
-                <NavLink className={({ isActive }) => (isActive ? "text-red-500 border border-1.2 p-2 rounded-2xl" : "")} to="/"> Home </NavLink>
-                <NavLink className={({ isActive }) => (isActive ? "text-red-500 border border-1.2 p-2 rounded-2xl" : "")} to="/shop"> Shop </NavLink>
-                <NavLink className={({ isActive }) => (isActive ? "text-red-500 border border-1.2 p-2 rounded-2xl" : "")} to="/about"> About </NavLink>
+                <NavLink className={({ isActive }) => (isActive ? "text-red-600 border border-1.2 p-2 rounded-2xl" : "")} to="/"> Home </NavLink>
+                <NavLink className={({ isActive }) => (isActive ? "text-red-600 border border-1.2 p-2 rounded-2xl" : "")} to="/shop"> Shop </NavLink>
+                <NavLink className={({ isActive }) => (isActive ? "text-red-6h00 border border-1.2 p-2 rounded-2xl" : "")} to="/about"> About </NavLink>
             </div>
               <div>
         <button>Login</button>
