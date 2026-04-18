f8a14e243a18608ff645adb0d4d6589ce04b3043
Shivam-136 <shivam243028@gmail.com>
2026-04-18 20:30:59 +0530

    Basic-Router-Project

--- React-Router-02/src/components/NavBar.jsx
+++ React-Router-02/src/components/NavBar.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import { NavLink } from 'react-router'
 
 const Navbar = () => {
-
-    
     return (
+
         <div className='flex justify-between items-centerp p-6 shadow-md bg-blue-700'>
             <h1 className='bg-black text-white px-2 p-2 rounded-2xl'>SHIVAM_K</h1>
             <div className='flex gap-10'>
