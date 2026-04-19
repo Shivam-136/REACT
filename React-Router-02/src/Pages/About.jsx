05ccd6068da3899ce03c09e3b48d7fcc9eadcc63
Shivam-136 <shivam243028@gmail.com>
2026-04-19 22:00:31 +0530

    Basic-Router-Project

--- React-Router-02/src/Pages/About.jsx
+++ React-Router-02/src/Pages/About.jsx
@@ -6,19 +6,23 @@ const Home = () => {
 
       {/* 🔥 Heading */}
       <div className="text-center   ">
+
         <h1 className="text-4xl font-bold   ">About Our Store 🛍️</h1>
         <p className="mt-4 text-gray-600 ">
+
           We provide the best products with top quality and affordable prices.
         </p>
       </div>
-
       {/* 💡 Features Section */}
       <div className="grid grid-cols-3 gap-6 text-center">
+
         <div className="p-5 shadow rounded border border-blue-800">
           <h2 className="text-xl font-bold ">🚚 Fast Delivery</h2>
           <p className="mt-2 text-sm text-gray-600">
             Get your products delivered quickly at your doorstep.
+            
           </p>
+
         </div>
 
         <div className="p-5 shadow rounded border border-blue-800">
@@ -40,7 +44,7 @@ const Home = () => {
       <div className="text-center bg-gray-100 p-6 rounded border border-blue-800">
         <h2 className="text-2xl font-bold">Contact Us</h2>
         <p className="mt-2">Email: support@store.com</p>
-        <p>Phone: +91 12345 67890</p>
+        <p>Phone: +91 12345 678990</p>
       </div>
 
     </div>
